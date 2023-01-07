8acf62ce92fad1f15e58aa7c8be28ce6dbca9d09
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2023-01-07 15:17:56 -0700

    authentication

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js";
 
 // Config
 const __filename = fileURLToPath(import.meta.url);
@@ -51,8 +52,10 @@ const upload = multer({
   storage,
 });
 
-// Mongoose Setup
+// Routes with files
+app.post("/auth/register", upload.single("picture"), register);
 
+// Mongoose Setup
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(`${process.env.MONGO_URL}`, {
